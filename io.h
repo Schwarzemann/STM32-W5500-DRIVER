e1a1b629246ccd028e51b16122ed341ee2741f14
Schwarzemann <barantbsd@gmail.com>
2024-01-30 21:30:10 +0300

    Simple header for I/O operations.

---
+++ io.h
@@ -0,0 +1,12 @@
+#ifndef IO_H
+#define IO_H
+
+#include <stdint.h>
+
+// I/O operations
+void outb(uint16_t port, uint8_t value);
+uint8_t inb(uint16_t port);
+void outw(uint16_t port, uint16_t value);
+uint16_t inw(uint16_t port);
+
+#endif // IO_H
