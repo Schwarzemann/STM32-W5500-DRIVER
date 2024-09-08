d3a55b4c5110a55abcde64564db0959bae2d8a33
Schwarzemann <barantbsd@gmail.com>
2024-09-08 21:49:36 +0300

    Break down _r8139dn_net_interrupt_tx function into multiple parts for better management

--- network_interrupt.c
+++ network_interrupt.c
@@ -32,39 +32,58 @@ irqreturn_t r8139dn_net_interrupt(int irq, void *dev) {
     return IRQ_HANDLED;
 }
 
+static void r8139dn_update_tx_stats(struct net_device *ndev, u32 tsd) {
+    if (tsd & TSD_TOK) {
+        // Successful transmission
+        ndev->stats.tx_packets++;
+        ndev->stats.tx_bytes += (tsd & TSD_SIZE);
+    } else {
+        // Transmission error
+        ndev->stats.tx_errors++;
+
+        if (tsd & TSD_TABT) {
+            ndev->stats.tx_aborted_errors++;
+        }
+
+        if (tsd & TSD_TUN) {
+            ndev->stats.tx_fifo_errors++;
+        }
+    }
+}
+
+static void r8139dn_handle_tx_interrupt_error(struct net_device *ndev, u32 tsd) {
+    r8139dn_w16(ISR, INT_TER);  // Write error interrupt
+    r8139dn_update_tx_stats(ndev, tsd);
+}
+
+static bool r8139dn_process_tx_descriptor(struct net_device *ndev, struct r8139dn_tx_ring *tx_ring, int hw) {
+    u32 tsd = r8139dn_r32(TSD0 + hw * TSD_GAP);
+
+    if (!(tsd & (TSD_TOK | TSD_TUN | TSD_TABT))) {
+        return false;  // No valid descriptor to process
+    }
+
+    if (tsd & TSD_TOK) {
+        r8139dn_update_tx_stats(ndev, tsd);
+    } else {
+        r8139dn_handle_tx_interrupt_error(ndev, tsd);
+    }
+
+    smp_store_release(&tx_ring->hw, (hw + 1) & (R8139DN_TX_DESC_NB - 1));
+    return true;
+}
+
 void _r8139dn_net_interrupt_tx(struct net_device *ndev) {
     struct r8139dn_priv *priv = netdev_priv(ndev);
     struct r8139dn_tx_ring *tx_ring = &priv->tx_ring;
-    int cpu, hw;
-    u32 tsd;
-
-    hw = smp_load_acquire(&tx_ring->hw);
-    cpu = smp_load_acquire(&tx_ring->cpu);
+    int hw = smp_load_acquire(&tx_ring->hw);
+    int cpu = smp_load_acquire(&tx_ring->cpu);
 
     while (hw != cpu) {
-        tsd = r8139dn_r32(TSD0 + hw * TSD_GAP);
-
-        if (!(tsd & (TSD_TOK | TSD_TUN | TSD_TABT))) {
+        if (!r8139dn_process_tx_descriptor(ndev, tx_ring, hw)) {
             break;
         }
-
-        if (tsd & TSD_TOK) {
-            ndev->stats.tx_packets++;
-            ndev->stats.tx_bytes += (tsd & TSD_SIZE);
-        } else {
-            r8139dn_w16(ISR, INT_TER);
-            ndev->stats.tx_errors++;
-
-            if (tsd & TSD_TABT) {
-                ndev->stats.tx_aborted_errors++;
-            }
-
-            if (tsd & TSD_TUN) {
-                ndev->stats.tx_fifo_errors++;
-            }
-        }
-
-        smp_store_release(&tx_ring->hw, (hw + 1) & (R8139DN_TX_DESC_NB - 1));
+        hw = smp_load_acquire(&tx_ring->hw);
     }
 
     if (netif_queue_stopped(ndev)) {
