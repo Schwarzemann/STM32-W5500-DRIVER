4add1daf9703fd1190c96aaa5ae8a3862ca1ad5e
Schwarzemann <barantbsd@gmail.com>
2024-09-25 00:19:14 +0300

    New new project

--- common.h
+++
@@ -1,8 +0,0 @@
-// Define a prefix for the log messages, making it easy to change the prefix in the future.
-#define LOG_PREFIX KBUILD_MODNAME ": "
-
-// Define a generic logging format function that takes a format string as input and prepends the log prefix.
-#define LOG_FORMAT(fmt) LOG_PREFIX fmt
-
-// Example usage of the LOG_FORMAT macro
-#define pr_fmt(fmt) LOG_FORMAT(fmt)
